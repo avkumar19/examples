4caa925d9884b7b8737c7ff0d1a2f8a6ee8342fa
Avinash Kumar <avkumar_19@hotmail.com>
2018-06-20 16:13:46 +0530

    Update guestbook.sh

--- guestbook.sh
+++ guestbook.sh
@@ -2,18 +2,15 @@
 
 #cloning the project sample repository
 sudo git clone https://github.com/avkumar19/examples.git
-cd ~/examples/guestbook
+cd examples/guestbook
 #Creating pods and services
-kubectl apply -f redis-master-deployment.yaml
-kubectl apply -f redis-master-service.yaml  
-kubectl apply -f redis-slave-deployment.yaml
-kubectl apply -f redis-slave-service.yaml
-kubectl apply -f frontend-deployment.yaml
-kubectl apply -f frontend-service.yaml
+kubectl apply -f <( istioctl kube-inject -f redis-master-deployment.yaml)
+kubectl apply -f <( istioctl kube-inject -f redis-master-service.yaml)
+kubectl apply -f <( istioctl kube-inject -f redis-slave-deployment.yaml)
+kubectl apply -f <( istioctl kube-inject -f redis-slave-service.yaml)
+kubectl apply -f <( istioctl kube-inject -f frontend-deployment.yaml)
+kubectl apply -f <( istioctl kube-inject -f frontend-service.yaml)
 #Checking running services
 kubectl get svc
 #Getting External Ip to access
 kubectl get svc frontend -o wide
-
-
-
