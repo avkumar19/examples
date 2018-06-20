cf00fe05e52f78c8149978ae37354a03f9d0f7ab
avkumar19 <avkumar_19@hotmail.com>
2018-06-20 09:57:15 +0000

    aws configure

---
+++ guestbook.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#cloning the project sample repository
+sudo git clone https://github.com/avkumar19/examples.git
+cd examples/guestbook
+#Creating pods and services
+kubectl apply -f redis-master-deployment.yaml
+kubectl apply -f redis-master-service.yaml  
+kubectl apply -f redis-slave-deployment.yaml
+kubectl apply -f redis-slave-service.yaml
+kubectl apply -f frontend-deployment.yaml
+kubectl apply -f frontend-service.yaml
+#Checking running services
+kubectl get svc
+#Getting External Ip to access
+kubectl get svc frontend -o wide
+
+
+
